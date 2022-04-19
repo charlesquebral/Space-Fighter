03aa274b2b6fccad6f6819c57e91cee63dc1bf68
Gevin Sarmiento <81723862+gevin-sarmiento@users.noreply.github.com>
2022-04-18 21:42:25 -0700

    Update ButtonClass.py
    I have refactored the Button Classes. I applied the inheritance. The ButtonImage class does not seem to work perfectly fine.

--- ButtonClass.py
+++ ButtonClass.py
@@ -1,61 +1,12 @@
 import pygame
 
-class Button():
-  def __init__(self, text, font, fontSize, text_color,x, y, button_color):
-    self.fontsize=fontSize
-    self.font = pygame.font.Font(font, fontSize)
-    self.textstring=text
-    self.text = self.font.render(text, True, text_color)
-    self.text_color=text_color
-    self.button_color=button_color
-
-    self.button_rect= pygame.Rect(x,y,150,50)
-    self.button_rect.centerx=x
-    self.textrect = self.text.get_rect()
-    self.textrect.centerx=self.button_rect.centerx
-    self.textrect.centery=self.button_rect.centery
-
- 
-    self.clicked = False
-
-
-
- 
-  def click(self, surface,events):
-
-    #get mouse position
-    mousePos = pygame.mouse.get_pos()
-     #draw button on screen
-
-
-
-    #check mouseover and clicked conditions
-
-    if self.button_rect.collidepoint(mousePos):
-      pygame.draw.rect(surface, (94,97,100), self.button_rect)
-      text = self.font.render(self.textstring, True, (0,0,0))
-      surface.blit(text, (self.textrect.x, self.textrect.y))
-      for event in events:
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button==1:
-              pygame.draw.rect(surface, self.button_color, self.button_rect)
-              surface.blit(self.text, (self.textrect.x, self.textrect.y))
-              self.clicked = True
-        else:
-          self.clicked = False
-    else:
-      pygame.draw.rect(surface, self.button_color, self.button_rect)
-      surface.blit(self.text, (self.textrect.x, self.textrect.y))
-    return self.clicked
-  
-
 class ButtonText():
-  def __init__(self, text, font, fontSize, color, x, y):
+  def __init__(self, text, font, fontSize, text_color, x, y):
     self.font = pygame.font.Font(font, fontSize)
-    self.text = self.font.render(text, True, color)
-    self.textrect = self.text.get_rect()
-    self.textrect.centerx = x
-    self.textrect.y = y  
+    self.text = self.font.render(text, True, text_color)
+    self.rect = self.text.get_rect()
+    self.rect.centerx = x
+    self.rect.y = y  
     self.clicked = False
 
   def click(self, surface,events):
@@ -64,41 +15,46 @@ class ButtonText():
     mousePos = pygame.mouse.get_pos()
 
     #draw button on screen
-    surface.blit(self.text, (self.textrect.x, self.textrect.y))
-    if self.textrect.collidepoint(mousePos):
-      for event in events:
-        if event.type == pygame.MOUSEBUTTONUP:
-          self.clicked = True
-        else:
-          self.clicked = False
-  
-    return self.clicked
-class ButtonImage():
-  def __init__(self, image, image_width, image_height, x, y):
-    self.image=image
-    self.image=pygame.transform.scale(self.image,(image_width,image_height)) #scale image
-    #self.rect=self.image.get_rect()
-    self.rect = pygame.Rect(x, y, image_width, image_height)
-    self.x=x
-    self.y=y
-
-    self.clicked = False
-
-  def click(self, surface,events):
-   
-    #get mouse position
-    mousePos = pygame.mouse.get_pos()
-     #draw button on screen
-    #pygame.draw.rect(surface, self.button_color, self.button_rect)
-    surface.blit(self.image, (self.x,self.y))
-    #check mouseover and clicked conditions
-   
+    surface.blit(self.text, (self.rect.x, self.rect.y))
     if self.rect.collidepoint(mousePos):
-    
       for event in events:
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
           self.clicked = True
         else:
           self.clicked = False
   
     return self.clicked
+
+class Button(ButtonText):
+  def __init__(self, text, font, fontSize, text_color, x, y, button_color):
+    super().__init__(text, font, fontSize, text_color, x, y)
+    
+    self.button_color=button_color
+    #self.textstring=text
+    self.rect= pygame.Rect(x,y,150,50) #this self.rect is different rect from the super class's self.rect
+    self.rect.centerx=x
+    self.textrect = self.text.get_rect()
+    self.textrect.centerx=self.rect.centerx
+    self.textrect.centery=self.rect.centery
+
+  def click(self, surface,events): 
+    self.clicked = super().click(surface, events)
+    mousePos = pygame.mouse.get_pos()
+    if self.rect.collidepoint(mousePos):
+      pygame.draw.rect(surface, (94,97,100), self.rect)
+      
+    else:
+      pygame.draw.rect(surface, self.button_color, self.rect)
+    
+    surface.blit(self.text, (self.textrect.x, self.textrect.y))
+
+    return self.clicked
+  
+
+class ButtonImage(ButtonText):
+  def __init__(self,image, image_width, image_height, x, y, text ='', font=None, fontSize=0, text_color=(255,255,255)):
+    super().__init__(text, font, fontSize, text_color, x, y )
+    self.text=pygame.transform.scale(image, (image_width,image_height)) #scale image
+    #self.rect=self.image.get_rect()
+    self.rect = self.text.get_rect()
+    self.rect.topleft = (x,y)
